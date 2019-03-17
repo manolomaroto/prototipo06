fea95498b1babb045ccaa6b2230989baca44b42a
Manuel Gonzalez Maroto <manuelgmaroto@gmail.com>
2019-03-17 18:50:03 +0100

    Añadido home

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+declare let $: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.jqueryCode();
+  }
+
+  jqueryCode() {
+    $(document).ready(() => {
+      $('.parallax').parallax();
+    });
+  }
+
+}
