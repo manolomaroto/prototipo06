4a41110be4c5fdb6100938ba61d86c9bf7a6fafa
Manuel Gonzalez Maroto <manuelgmaroto@gmail.com>
2019-06-23 13:47:25 +0200

    Añadido estructura despachos

--- src/app/services/localizacion.service.ts
+++ src/app/services/localizacion.service.ts
@@ -6,7 +6,11 @@ export class LocalizacionService {
     private localizaciones: any[] = [{
         title: 'Miraflores'
     }, {
-        title: 'Madrid'
+        title: 'Madrid',
+        descripcion: `Gracias a su magnífica ubicación en la Calle Goya, en el corazón del Barrio de Salamanca,
+        es muy sencillo su acceso desde diferentes puntos de la ciudad a través de metro (estación metro Goya,
+        líneas 2 y 4), múltiples autobuses, taxis y parkings en las inmediaciones.`,
+        fotos : ['../../assets/img/bulling.jpg'],
     }, {
         title: 'Colmenar'
     }];
